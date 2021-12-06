352d0c742c7b7b7cdf1b5d847caf70e8a2b3453a
Philipp Kochanski <kochanskip@outlook.com>
2021-12-06 13:32:25 +0100

    Solution for Day 6

---
+++ day_06/task_1.py
@@ -0,0 +1,16 @@
+lanternfish = list(map(int, open("test_input.txt", "r").readline().strip().split(",")))
+# index describes day - with 9 possible days - 0,1,2,3,4,5,6,7,8 
+by_day = [lanternfish.count(i) for i in range(9)]
+
+for i in range(80):
+    # by popping from the front we "substract" one day from each element from the rest of the list
+    fish_num = by_day.pop(0)
+    
+    # add old fish to start at day 6 again
+    by_day[6] += fish_num
+    
+    # add new fish to start at day 8
+    by_day.append(fish_num)
+    
+    
+print(f"Fish after 80 days: {sum(by_day)}")
---
+++ day_06/task_2.py
@@ -0,0 +1,16 @@
+lanternfish = list(map(int, open("test_input.txt", "r").readline().strip().split(",")))
+# index describes day - with 9 possible days - 0,1,2,3,4,5,6,7,8 
+by_day = [lanternfish.count(i) for i in range(9)]
+
+for i in range(256):
+    # by popping from the front we move each element by one day (subscract one day)
+    fish_num = by_day.pop(0)
+    
+    # add old fish to start at day 6 again
+    by_day[6] += fish_num
+    
+    # add new fish to start at day 8
+    by_day.append(fish_num)
+    
+    
+print(f"Fish after 256 days: {sum(by_day)}")
