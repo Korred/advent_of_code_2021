7f792378a849d14b15cc5d345c34c2a024bfd432
Philipp Kochanski <kochanskip@outlook.com>
2021-12-17 11:43:48 +0100

    Solution for Day 16

--- day_16/task_1.py
+++ day_16/task_1.py
@@ -1,32 +1,73 @@
-integer = "D2FE28"
-bin_rep = f'{int(integer, 16):0>{len(integer)*4}b}'
-
-p_version = int(bin_rep[0:3].zfill(4), 2)
-p_type = int(bin_rep[3:6].zfill(4), 2)
-
-print(p_version, p_type)
-
-# literal value packet
-if p_type == 4:
-    groups = []
-    pos = 6
-    while True:
-        group = bin_rep[pos:pos+5]
-        print(group)
-        groups.append(group[1:])
-        # last group - stop parsing
-        if group[0] == "0":
-            break
-        
-        pos += 5
-            
-    p_val = int("".join(groups), 2)
-    
-    
-# operator packet
-else:
-    pass
+def hexstr_to_binstr(hex):
+    return f"{int(hex, 16):0>{len(hex)*4}b}"
 
 
-print(p_val)
-    
+def decode_packet(packet):
+
+    # cosume packet version
+    p_version = int(packet[:3], 2)
+    p_version_sum = p_version
+    packet = packet[3:]
+
+    # consume packet type
+    p_type = int(packet[:3], 2)
+    packet = packet[3:]
+
+    # literal value packet
+    if p_type == 4:
+        groups = []
+        while True:
+            # consume literal value group
+            group = packet[1:5]
+            num = packet[0]
+            groups.append(group)
+            packet = packet[5:]
+
+            # last group - stop parsing
+            if num == "0":
+                break
+
+        p_val = int("".join(groups), 2)
+        return {"remaining": packet, "value": p_val, "version_sum": p_version_sum}
+
+    # operator packet
+    else:
+        # consume operator id
+        p_id = packet[0]
+        packet = packet[1:]
+
+        subpacket_vals = []
+
+        if p_id == "0":
+            # consume subpack length information
+            p_total_length = int(packet[:15], 2)
+            packet = packet[15:]
+
+            # consume subpackages
+            subpackets = packet[:p_total_length]
+            while subpackets:
+                res = decode_packet(subpackets)
+                subpackets = res["remaining"]
+                subpacket_vals.append(res["value"])
+                p_version_sum += res["version_sum"]
+
+            packet = packet[p_total_length:]
+
+        else:
+            # consume subpackages num
+            p_subpack_num = int(packet[:11], 2)
+            packet = packet[11:]
+
+            # consume subpackages
+            for _ in range(p_subpack_num):
+                res = decode_packet(packet)
+                packet = res["remaining"]
+                subpacket_vals.append(res["value"])
+                p_version_sum += res["version_sum"]
+
+        return {"remaining": packet, "value": 0, "version_sum": p_version_sum}
+
+
+bin_packet = hexstr_to_binstr(open("input.txt", "r").readline())
+print(decode_packet(bin_packet))
+
---
+++ day_16/task_2.py
@@ -0,0 +1,97 @@
+from math import prod
+
+
+def hexstr_to_binstr(hex):
+    return f"{int(hex, 16):0>{len(hex)*4}b}"
+
+
+def decode_packet(packet):
+
+    # cosume packet version
+    p_version = int(packet[:3], 2)
+    p_version_sum = p_version
+    packet = packet[3:]
+
+    # consume packet type
+    p_type = int(packet[:3], 2)
+    packet = packet[3:]
+
+    # literal value packet
+    if p_type == 4:
+        groups = []
+        while True:
+            # consume literal value group
+            group = packet[1:5]
+            num = packet[0]
+            groups.append(group)
+            packet = packet[5:]
+
+            # last group - stop parsing
+            if num == "0":
+                break
+
+        p_val = int("".join(groups), 2)
+        return {"remaining": packet, "value": p_val, "version_sum": p_version_sum}
+
+    # operator packet
+    else:
+        # consume operator id
+        p_id = packet[0]
+        packet = packet[1:]
+
+        subpacket_vals = []
+
+        if p_id == "0":
+            # consume subpack length information
+            p_total_length = int(packet[:15], 2)
+            packet = packet[15:]
+
+            # consume subpackages
+            subpackets = packet[:p_total_length]
+            while subpackets:
+                res = decode_packet(subpackets)
+                subpackets = res["remaining"]
+                subpacket_vals.append(res["value"])
+                p_version_sum += res["version_sum"]
+
+            packet = packet[p_total_length:]
+
+        else:
+            # consume subpackages num
+            p_subpack_num = int(packet[:11], 2)
+            packet = packet[11:]
+
+            # consume subpackages
+            for _ in range(p_subpack_num):
+                res = decode_packet(packet)
+                packet = res["remaining"]
+                subpacket_vals.append(res["value"])
+                p_version_sum += res["version_sum"]
+
+        if p_type == 0:
+            value = sum(subpacket_vals)
+
+        elif p_type == 1:
+            value = prod(subpacket_vals)
+
+        elif p_type == 2:
+            value = min(subpacket_vals)
+
+        elif p_type == 3:
+            value = max(subpacket_vals)
+
+        elif p_type == 5:
+            value = 1 if subpacket_vals[0] > subpacket_vals[1] else 0
+
+        elif p_type == 6:
+            value = 1 if subpacket_vals[0] < subpacket_vals[1] else 0
+
+        elif p_type == 7:
+            value = 1 if subpacket_vals[0] == subpacket_vals[1] else 0
+
+        return {"remaining": packet, "value": value, "version_sum": p_version_sum}
+
+
+bin_packet = hexstr_to_binstr(open("input.txt", "r").readline())
+print(decode_packet(bin_packet))
+
